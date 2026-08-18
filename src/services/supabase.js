2e1ff6fe4db454c3999adef325af14c0a696f1fb
nicy24782057 <aurawarior@gmail.com>
2026-08-18 22:24:24 +0700

    fix github pages environment

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,6 +3,9 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 
+console.log("URL", supabaseUrl)
+console.log("KEY", supabaseKey)
+
 if (!supabaseUrl || !supabaseKey) {
   throw new Error(
     'Supabase URL atau Publishable Key belum terbaca dari .env.local'
